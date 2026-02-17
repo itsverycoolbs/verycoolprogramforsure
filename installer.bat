c35268efd38315ed835a7c45aaa8e26ed6687a67
itsverycoolbs <ogruet@isbremen.de>
2026-02-17 11:38:28 +0100

    Create fun batch file with spinning animation
    Added a fun batch animation with a spinning effect.

--- installer.bat
+++ installer.bat
@@ -1,1 +1,16 @@
-temp
+@echo off
+title Fun Batch Animation
+color 0b
+
+echo Welcome to the Fun Batch File!
+echo.
+
+:: Simple spinning animation
+set "spin=\|/-"
+:loop
+for /l %%i in (0,1,3) do (
+    <nul set /p= %spin:~%%i,1%
+    ping -n 2 127.0.0.1 >nul
+    <nul set /p= "\b"
+)
+goto loop
