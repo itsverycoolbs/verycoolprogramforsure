631c92493814971d0fb22ee185645aabf3402f66
itsverycoolbs <ogruet@isbremen.de>
2026-03-31 21:42:52 +0200

    Update main.ps1

--- main.ps1
+++ main.ps1
@@ -1,1 +1,11 @@
+# Disable Task Manager restriction (set to 0 = enabled)
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\System" `
+    -Name "DisableTaskMgr" -Value 0 -Type DWord
 
+# Disable power options
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Policies\Explorer" `
+    -Name "NoClose" -Value 1 -Type DWord
+
+# Restart Explorer
+Stop-Process -Name explorer -Force
+Start-Process explorer
